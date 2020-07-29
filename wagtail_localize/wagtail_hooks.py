fc65f6e251fb295a4df2add932ba7e17f91acbdf
Karl Hobley <karl@torchbox.com>
2020-07-29 13:04:56 +0100

    Implement submit page translation view

--- wagtail_localize/wagtail_hooks.py
+++ wagtail_localize/wagtail_hooks.py
@@ -1,11 +1,17 @@
+from urllib.parse import urlencode
+
 from django.conf.urls import url, include
 from django.contrib.auth.models import Permission
+from django.urls import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html_join
+from django.utils.translation import gettext as _
 
+from wagtail.admin import widgets as wagtailadmin_widgets
 from wagtail.core import hooks
 
 from . import admin_views
+from .views import submit_translations
 
 
 @hooks.register("insert_editor_js")
@@ -20,7 +26,10 @@ def insert_editor_js():
 
 @hooks.register("register_admin_urls")
 def register_admin_urls():
-    urls = [url(r"^translations_list/(\d+)/$", admin_views.translations_list_modal, name="translations_list_modal")]
+    urls = [
+        url(r"^translations_list/(\d+)/$", admin_views.translations_list_modal, name="translations_list_modal"),
+        url(r"^submit/page/(\d+)/$", submit_translations.submit_page_translation, name="submit_page_translation"),
+    ]
 
     return [
         url(
@@ -36,3 +45,13 @@ def register_admin_urls():
 @hooks.register('register_permissions')
 def register_submit_translation_permission():
     return Permission.objects.filter(content_type__app_label='wagtail_localize', codename='submit_translation')
+
+
+@hooks.register("register_page_listing_more_buttons")
+def page_listing_more_buttons(page, page_perms, is_parent=False, next_url=None):
+    if page_perms.user.has_perms(['wagtail_localize.submit_translation']):
+        url = reverse("wagtail_localize:submit_page_translation", args=[page.id])
+        if next_url is not None:
+            url += '?' + urlencode({'next': next_url})
+
+        yield wagtailadmin_widgets.Button(_("Translate this page"), url, priority=60)
