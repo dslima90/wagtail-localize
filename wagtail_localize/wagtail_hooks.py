ca2e3d8f53f6a9eef9802566174d2bc2c8286f79
Karl Hobley <karl@torchbox.com>
2020-07-30 08:40:07 +0100

    Initial work on edit translation view

--- wagtail_localize/wagtail_hooks.py
+++ wagtail_localize/wagtail_hooks.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
 from django.templatetags.static import static
+from django.urls import path
 from django.utils.html import format_html_join
+from django.views.i18n import JavaScriptCatalog
 
 from wagtail.core import hooks
+from wagtail.core.models import TranslatableMixin
 
 from . import admin_views
+from .models import Translation
+from .views.edit_translation import edit_translation
 
 
 @hooks.register("insert_editor_js")
@@ -19,7 +24,10 @@ def insert_editor_js():
 
 @hooks.register("register_admin_urls")
 def register_admin_urls():
-    urls = [url(r"^translations_list/(\d+)/$", admin_views.translations_list_modal, name="translations_list_modal")]
+    urls = [
+        path('jsi18n/', JavaScriptCatalog.as_view(packages=['wagtail_localize']), name='javascript_catalog'),
+        url(r"^translations_list/(\d+)/$", admin_views.translations_list_modal, name="translations_list_modal"),
+    ]
 
     return [
         url(
@@ -30,3 +38,26 @@ def register_admin_urls():
             ),
         )
     ]
+
+
+@hooks.register("before_edit_page")
+def before_edit_page(request, page):
+    # Overrides the edit page view if the page is the target of a translation
+    try:
+        translation = Translation.objects.get(object_id=page.translation_key, target_locale_id=page.locale_id, enabled=True)
+        return edit_translation(request, translation, page)
+
+    except Translation.DoesNotExist:
+        pass
+
+
+@hooks.register("before_edit_snippet")
+def before_edit_page(request, instance):
+    # Overrides the edit snippet view if the snippet is translatable and the target of a translation
+    if isinstance(instance, TranslatableMixin):
+        try:
+            translation = Translation.objects.get(object_id=instance.translation_key, target_locale_id=instance.locale_id, enabled=True)
+            return edit_translation(request, translation, instance)
+
+        except Translation.DoesNotExist:
+            pass
