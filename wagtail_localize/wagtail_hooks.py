55e6c04955b92565c9485c01338069eab9d6d8f0
Karl Hobley <karl@torchbox.com>
2020-07-31 21:04:44 +0100

    Initial work on report

--- wagtail_localize/wagtail_hooks.py
+++ wagtail_localize/wagtail_hooks.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url, include
 from django.templatetags.static import static
-from django.urls import path
+from django.urls import path, reverse
 from django.utils.html import format_html_join
+from django.utils.translation import ugettext_lazy as _
 from django.views.i18n import JavaScriptCatalog
-
+from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
 from wagtail.core.models import TranslatableMixin
 
 from . import admin_views
 from .models import Translation
+from .views.api import ListTranslationsView
 from .views.edit_translation import edit_translation
+from .views.reports import translations_report
 
 
 @hooks.register("insert_editor_js")
@@ -24,9 +27,15 @@ def insert_editor_js():
 
 @hooks.register("register_admin_urls")
 def register_admin_urls():
+    api_urls = [
+        path('translations/', ListTranslationsView.as_view(), name='translations'),
+    ]
+
     urls = [
+        path('api/', include((api_urls, "api"), namespace="api")),
         path('jsi18n/', JavaScriptCatalog.as_view(packages=['wagtail_localize']), name='javascript_catalog'),
         url(r"^translations_list/(\d+)/$", admin_views.translations_list_modal, name="translations_list_modal"),
+        path('reports/translations/', translations_report, name='translations_report'),
     ]
 
     return [
@@ -61,3 +70,18 @@ def before_edit_page(request, instance):
 
         except Translation.DoesNotExist:
             pass
+
+
+class TranslationsMenuItem(MenuItem):
+    def is_shown(self, request):
+        return True
+
+
+@hooks.register("register_reports_menu_item")
+def register_menu_item():
+    return TranslationsMenuItem(
+        _("Translations"),
+        reverse("wagtail_localize:translations_report"),
+        classnames="icon icon-site",
+        order=500,
+    )
